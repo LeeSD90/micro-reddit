6e5c43095e4c2e08ff5b2d40f6ddf40e95f36cad
LeeSD90 <lee.doyle90@gmail.com>
2018-04-25 20:44:37 +0200

    Add Post model and db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_25_135406) do
+ActiveRecord::Schema.define(version: 2018_04_25_184359) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
