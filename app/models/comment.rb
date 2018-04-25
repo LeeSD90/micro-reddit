e9eae9967c92f6b278514a015511e1838e089a51
LeeSD90 <lee.doyle90@gmail.com>
2018-04-25 21:15:50 +0200

    Add validation of FKs to Post and Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
 	validates :content, presence: true, length: { maximum: 500 }
 	belongs_to :user
 	belongs_to :post
+	validates :user_id, presence: true
+  	validates :post_id, presence: true
 end
