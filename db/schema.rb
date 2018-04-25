417cf1cf8ff4c79d9e426f06c8401328595f1bcf
LeeSD90 <lee.doyle90@gmail.com>
2018-04-25 21:17:28 +0200

    Db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_25_184917) do
+ActiveRecord::Schema.define(version: 2018_04_25_191028) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
