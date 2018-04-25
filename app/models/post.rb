2cd501111e11163f4d169063504756b79e00c72b
LeeSD90 <lee.doyle90@gmail.com>
2018-04-25 21:01:21 +0200

    Add FK user_id to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+	validates :title, presence: true, length: { maximum: 80 }
+	validates :content, presence: true, length: { maximum: 2000 }
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_25_184359) do
+ActiveRecord::Schema.define(version: 2018_04_25_184917) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
