a2dad94cb624c79ac16f1c516c1945fde8425dc4
LeeSD90 <lee.doyle90@gmail.com>
2018-04-25 21:13:22 +0200

    Add Comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
 	validates :title, presence: true, length: { maximum: 80 }
 	validates :content, presence: true, length: { maximum: 2000 }
 	belongs_to :user
+	has_many :comments
 end
